0408255a080f04ccbf3a9c4b9220988c9429fb02
star-Elmi <istarmohamed503@gmail.com>
2025-04-19 21:03:38 +0300

    updated chatinput

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -22,7 +22,7 @@ const ChatInput = ({ onSend }) => {
       <button onClick={handleSend} className="bg-blue-600 text-white px-4 py-2 rounded">
         Sends
       </button>
-      <button>voice</button>
+      
     </div>
   );
 };
