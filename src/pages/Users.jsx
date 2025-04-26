55035e152faa9b5265c968299efaad3df27c5500
star-Elmi <istarmohamed503@gmail.com>
2025-04-26 12:25:27 +0300

    updated users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -181,7 +181,9 @@ const UsersPage = () => {
         </Card>
       ))}
       ......
-      
+      ..console.log('UsersPage component rendered');
+console.log('Users data:', users);
+console.log('Handle delete function called with id:', id);
     </div>
   );
 };
