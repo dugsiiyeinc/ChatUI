a1498acd62c47dd49283c1c0895d137133505a35
star-Elmi <istarmohamed503@gmail.com>
2025-04-30 12:35:11 +0300

    Fix syntax error in HistoryIntro.jsx

--- src/components/HistoryIntro.jsx
+++ src/components/HistoryIntro.jsx
@@ -50,7 +50,7 @@ const HistoryIntro = () => {
             <li><strong>Xusuusin & Dib u Eegis:</strong> Waxay kaa caawin kartaa inaad horumar ka sameyso barashada, la-tashiga ama adeegyada aad isticmaalayso.</li>
           </ul>
         </CardContent>
-      </Card>
+      </Card>..
     </div>
   );
 };
