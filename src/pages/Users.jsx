6ba990a98a6c3283ad5b088dbff91a2ef47db5f3
star-Elmi <istarmohamed503@gmail.com>
2025-04-30 11:56:47 +0300

    correct the path Card

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Card, CardContent } from "../components/ui/card";
+import { Card } from '../components/ui/Card';
+
 import { Button } from "../components/ui/Button";
 import { Star } from "lucide-react";
 
@@ -180,7 +181,7 @@ const UsersPage = () => {
           </CardContent>
         </Card>
       ))}
-      
+      ....
  
     </div>
   );
