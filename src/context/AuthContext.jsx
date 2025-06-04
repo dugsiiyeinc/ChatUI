77516e9c64ab4011d991be71681cbaa92a7556f3
star-Elmi <istarmohamed503@gmail.com>
2025-06-04 22:31:02 +0300

    added DarmodeProvider

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -72,12 +72,12 @@ export const AuthProvider = ({ children }) => {
 };
 
 // ✅ Custom hook
-// export const useAuth = () => {
-//   const context = useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
 
-//   if (context === null) {
-//     throw new Error("useAuth must be used within an AuthProvider");
-//   }
+  if (context === null) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
 
-//   return context;
-// };
+  return context;
+};
